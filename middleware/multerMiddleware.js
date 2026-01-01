524f5c1de7a7cbb79c4214b19b85f719a7d56c96
Kelvin1237 <kelvinagyeman11@gmail.com>
2026-01-01 17:23:49 +0000

    fixed upload image

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -14,3 +14,21 @@ export const formatImage = (file) => {
 };
 
 export default upload;
+
+/* For disk storage 
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    const fileName = file.originalname;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload; */
